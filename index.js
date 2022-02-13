35a550ccce6c0e456596e036f5338b14aba93cdb
Nabegon <watami03@gmail.com>
2022-02-13 13:01:29 +0100

    add: multiple questions, oop-style, and it is now possible to get the total score of the correct answers.

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
+const questions = require('./questions');
 const { Quiz } = require('enquirer');
 
- const prompt = new Quiz({
-  name: 'modalpartikeln',
-  message: 'Wie geht es dir ___ ?',
-  choices: ['eigentlich', 'denn', 'doch', 'ja', 'ruhig'],
-  correctChoice: 1
-});
 
- prompt
-  .run(console.log('Let\'s start the Modalpartikel-Quiz!\nModalpertikel: aber, eben/halt, einfach, ja, ruhig, vielleicht, mal, eigentlich, denn, doch.' ))
-  .then(answer => {
+class SetQuiz {
+constructor(){
+  this.total = 0;
+}
+
+  introduce () {
+    console.log('Let\'s start the Modalpartikel-Quiz!\nModalpertikel: aber, eben/halt, einfach, ja, ruhig, vielleicht, mal, eigentlich, denn, doch.')
+  }
+
+  async set (number) {
+    const answer = await new Quiz(questions.questions[number]).run()
     if (answer.correct) {
-      console.log('Richtig! - Wie geht es dir denn?\ndenn - nue bei Fragen (situatives Interesse)');
+      this.total += 1;
+      if (answer.correctAnswer === 'denn'){
+        console.log('Richtig!\nWie geht es dir denn?\ndenn - nur bei Fragen(situative Interesse)')
+      }else if(answer.correctAnswer === 'ruhig'){
+        console.log('Richtig!\nDu kannst ruhig so viel Jägermeister trinken, wie du willst.\nruhig - kein Problem')
+      }else{
+        console.log('Richtig!\nWollen wir Döner essen? Nein, ich habe kein hunger. Dann gehe ich eben/halt nach Hause.\neben/halt - Allgemeingültigkeit, Resignation')
+      }
     } else {
-      console.log(`Falsch! Die richtige Antwort ist ${answer.correctAnswer}.\nWie geht es dir denn?\ndenn - nue bei Fragen (situatives Interesse)`);
+      if (answer.correctAnswer === 'denn'){
+        console.log(`Falsch! Die richtige Antwort ist ${answer.correctAnswer}.\nWie geht es dir denn?\ndenn - nue bei Fragen (situatives Interesse)`)
+      }else if(answer.correctAnswer === 'ruhig'){
+        console.log(`Falsch! Die richtige Antwort ist ${answer.correctAnswer}.\nDu kannst ruhig so viel Jägermeister trinken, wie du willst.\nruhig - kein Problem`)
+      }else{
+        console.log(`Falsch! Die richtige Antwort ist ${answer.correctAnswer}.\nWollen wir Döner essen? Nein, ich habe kein hunger. Dann gehe ich eben/halt nach Hause.\neben/halt - Allgemeingültigkeit, Resignatio`)
+      }
     }
-  })
-  .catch(console.error);
+  }
+
+  close(sumOfPoints){
+    console.log(`Your points are ${sumOfPoints}. Fertig :)`)
+  }
+}
+
+async function main() {
+  const quiz = new SetQuiz()
+  quiz.introduce()
+  for (let step = 0; step < 3; step++) {
+    await quiz.set(step)
+  }
+  quiz.close(quiz.total)
+}
+
+main();
---
+++ questions.js
@@ -0,0 +1,24 @@
+const questions = [
+    {
+      name: 'modalpartikel',
+      message: 'Wie geht es dir ___ ?',
+      choices: ['eigentlich', 'denn', 'doch', 'ja', 'ruhig'],
+      correctChoice: 1
+    },
+    {
+      name: 'modalpartikel',
+      message: 'Du kannst ___ so viel Jägermeister trinken, wie du willst.',
+      choices: ['doch', 'aber', 'eigentlich', 'ruhig', 'ja'],
+      correctChoice: 3
+    },
+    {
+      name: 'modalpartikel',
+      message: 'Wollen wir Döner essen? Nein, ich habe kein hunger. Dann gehe ich ___ nach Hause.',
+      choices: ['denn', 'vielleicht', 'mal', 'eben/halt', 'mal'],
+      correctChoice: 3
+    }
+  ]
+  
+  module.exports = {
+    questions
+  }
