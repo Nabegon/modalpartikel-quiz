ccde11a2211357c1561def75b1e1a0f39cb75085
Nabegon <watami03@gmail.com>
2022-02-15 16:29:01 +0100

    modified: eslint

--- index.js
+++ index.js
@@ -1,50 +1,37 @@
-const questions = require('./questions');
-const { Quiz } = require('enquirer');
-
-
+const { Quiz } = require('enquirer')
+const questions = require('./questions')
+const correctAnswerMessages = require('./correct_answer_messages')
+const wrongAnswerMessages = require('./wrong_answer_messages')
 class SetQuiz {
-constructor(){
-  this.total = 0;
-}
+  constructor () {
+    this.total = 0
+  }
 
   introduce () {
-    console.log('Let\'s start the Modalpartikel-Quiz!\nModalpertikel: aber, eben/halt, einfach, ja, ruhig, vielleicht, mal, eigentlich, denn, doch.')
+    console.log('Let\'s start the Modalpartikel-Quiz!\nModalpertikeln: aber, halt, einfach, ja, ruhig, vielleicht, mal, eigentlich, denn, doch.')
   }
 
   async set (number) {
     const answer = await new Quiz(questions.questions[number]).run()
     if (answer.correct) {
-      this.total += 1;
-      if (answer.correctAnswer === 'denn'){
-        console.log('Richtig!\nWie geht es dir denn?\ndenn - nur bei Fragen(situative Interesse)')
-      }else if(answer.correctAnswer === 'ruhig'){
-        console.log('Richtig!\nDu kannst ruhig so viel Jägermeister trinken, wie du willst.\nruhig - kein Problem')
-      }else{
-        console.log('Richtig!\nWollen wir Döner essen? Nein, ich habe kein hunger. Dann gehe ich eben/halt nach Hause.\neben/halt - Allgemeingültigkeit, Resignation')
-      }
+      this.total += 1
+      console.log(correctAnswerMessages.modalpartikeln[answer.correctAnswer])
     } else {
-      if (answer.correctAnswer === 'denn'){
-        console.log(`Falsch! Die richtige Antwort ist ${answer.correctAnswer}.\nWie geht es dir denn?\ndenn - nue bei Fragen (situatives Interesse)`)
-      }else if(answer.correctAnswer === 'ruhig'){
-        console.log(`Falsch! Die richtige Antwort ist ${answer.correctAnswer}.\nDu kannst ruhig so viel Jägermeister trinken, wie du willst.\nruhig - kein Problem`)
-      }else{
-        console.log(`Falsch! Die richtige Antwort ist ${answer.correctAnswer}.\nWollen wir Döner essen? Nein, ich habe kein hunger. Dann gehe ich eben/halt nach Hause.\neben/halt - Allgemeingültigkeit, Resignatio`)
-      }
+      console.log(wrongAnswerMessages.modalpartikeln[answer.correctAnswer])
     }
   }
 
-  close(sumOfPoints){
-    console.log(`Your points are ${sumOfPoints}. Fertig :)`)
+  close (sumOfPoints) {
+    console.log(`\nDu hast ${sumOfPoints} Fragen richtig beantwortet! :)`)
   }
 }
 
-async function main() {
+async function main () {
   const quiz = new SetQuiz()
   quiz.introduce()
-  for (let step = 0; step < 3; step++) {
+  for (let step = 0; step < 10; step += 1) {
     await quiz.set(step)
   }
   quiz.close(quiz.total)
 }
-
-main();
+main()
--- questions.js
+++ questions.js
@@ -1,24 +1,66 @@
 const questions = [
-    {
-      name: 'modalpartikel',
-      message: 'Wie geht es dir ___ ?',
-      choices: ['eigentlich', 'denn', 'doch', 'ja', 'ruhig'],
-      correctChoice: 1
-    },
-    {
-      name: 'modalpartikel',
-      message: 'Du kannst ___ so viel Jägermeister trinken, wie du willst.',
-      choices: ['doch', 'aber', 'eigentlich', 'ruhig', 'ja'],
-      correctChoice: 3
-    },
-    {
-      name: 'modalpartikel',
-      message: 'Wollen wir Döner essen? Nein, ich habe kein hunger. Dann gehe ich ___ nach Hause.',
-      choices: ['denn', 'vielleicht', 'mal', 'eben/halt', 'mal'],
-      correctChoice: 3
-    }
-  ]
-  
-  module.exports = {
-    questions
+  {
+    name: 'modalpartikel',
+    message: 'Wie geht es dir ___ ?',
+    choices: ['eigentlich', 'denn', 'doch', 'ja', 'ruhig'],
+    correctChoice: 1
+  },
+  {
+    name: 'modalpartikel',
+    message: 'Du kannst ___ so viel Jägermeister trinken, wie du willst.',
+    choices: ['doch', 'aber', 'eigentlich', 'ruhig', 'ja'],
+    correctChoice: 3
+  },
+  {
+    name: 'modalpartikel',
+    message: 'Wollen wir Döner essen? Nein, ich habe kein hunger. Dann gehe ich ___ nach Hause.',
+    choices: ['denn', 'vielleicht', 'mal', 'halt', 'ja'],
+    correctChoice: 3
+  },
+  {
+    name: 'modalpartikel',
+    message: 'Deutschland ist ___ toll',
+    choices: ['doch', 'vielleicht', 'eigentlich', 'halt', 'aber'],
+    correctChoice: 4
+  },
+  {
+    name: 'modalpartikel',
+    message: 'Er lief ___ davon.',
+    choices: ['denn', 'vielleicht', 'ruhig', 'einfach', 'doch'],
+    correctChoice: 3
+  },
+  {
+    name: 'modalpartikel',
+    message: 'Darüber haben wir ___ schon besprochen.',
+    choices: ['ja', 'vielleicht', 'doch', 'mal', 'halt'],
+    correctChoice: 0
+  },
+  {
+    name: 'modalpartikel',
+    message: 'Die Party war ___ toll.',
+    choices: ['doch', 'vielleicht', 'mal', 'halt', 'ja'],
+    correctChoice: 1
+  },
+  {
+    name: 'modalpartikel',
+    message: 'kannst du mir ___ helfen?',
+    choices: ['denn', 'aber', 'mal', 'halt', 'ruhig'],
+    correctChoice: 2
+  },
+  {
+    name: 'modalpartikel',
+    message: 'Hast du den Professor ___ schon gefragt?',
+    choices: ['einfach', 'vielleicht', 'eigentlich', 'ja', 'denn'],
+    correctChoice: 2
+  },
+  {
+    name: 'modalpartikel',
+    message: 'Vielleicht bestellen wir ___ lieber Suhi?',
+    choices: ['mal', 'vielleicht', 'denn', 'aber', 'doch'],
+    correctChoice: 4
   }
+]
+
+module.exports = {
+  questions
+}
