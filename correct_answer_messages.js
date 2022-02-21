1689b1162ddb5be6e64a47e86909adc8f8c35c1f
Nabegon <watami03@gmail.com>
2022-02-21 15:17:33 +0100

    modified: some answers are changed to match the PR.

--- correct_answer_messages.js
+++ correct_answer_messages.js
@@ -1,14 +1,14 @@
 const modalpartikeln = {
-  denn: 'Richtig!\nWie geht es dir denn?\ndenn - nur bei Fragen(situative Interesse)',
-  ruhig: 'Richtig!\nDu kannst ruhig so viel Jägermeister trinken, wie du willst.\nruhig - kein Problem',
-  halt: 'Richtig!\nWollen wir Döner essen? Nein, ich habe kein hunger. Dann gehe ich halt nach Hause.\neben/halt - Allgemeingültigkeit, Resignation',
-  aber: 'Richtig!\nDeutschland ist aber toll!\naber - sehr + adj (sehr/wirklich)',
-  einfach: 'Richtig!\nEr lief einfach davon.\neinfach - ohne Weiteres/ ohne weiteren Grund macht man so',
-  ja: 'Richtig!\nDarüber haben wir ja schon besprochen.\nja - Was bereits gesagt wurde',
-  vielleicht: 'Richtig!\nDie Party war vielleicht toll.\nvielleicht - in der Tat sehr + adj. Das bekräftigt man seine Meinung',
+  denn: 'Richtig!\nDanke, mir geht es gut! Wie geht es dir denn?\ndenn - nur bei Fragen(situative Interesse)',
+  ruhig: 'Richtig!\nWir haben die Getränke-Flatrate bestellt. nDu kannst ruhig so viel Jägermeister trinken, wie du willst.\nruhig - kein Problem',
+  halt: 'Richtig!\nWollen wir noch einen Döner essen? - Nein, ich habe keinen Hunger. - Dann gehe ich halt nach Hause.\nhalt - Allgemeingültigkeit, Resignation',
+  aber: 'Richtig!\nEin Bier kostet nur 50 Cent? Deutschland ist aber toll!\naber - sehr + adj (sehr/wirklich)',
+  einfach: 'Richtig!\nIch wollte mit ihm reden, aber er lief einfach weg.\neinfach - ohne Weiteres/ ohne weiteren Grund macht man so',
+  ja: 'Richtig!\nUnser Thema heute ist der Klimawandel. Darüber haben wir ja schon letzte Woche gesprochen.\nja - Was bereits gesagt wurde',
+  vielleicht: 'Richtig!\nWie war es gestern? - Die Party war vielleicht toll. Wir haben die ganze Nacht gefeiert.\nvielleicht - in der Tat sehr + adj. Das bekräftigt man seine Meinung',
   mal: 'Richtig!\nkannst du mir mal helfen?\nmal - höfliches Imperativ',
   eigentlich: 'Richtig!\nHast du den Professor eigentlich schon gefragt?\neigentlich - Themawechsel',
-  doch: 'Richtig!\nVielleicht bestellen wir doch lieber Suhi?\ndoch - freundliche Bitten und Aufforderungen'
+  doch: 'Richtig!\nHmm, Pizza ist lecker, aber Inga mag gerne Reis. Vielleicht bestellen wir doch lieber Suhi?\ndoch - freundliche Bitten und Aufforderungen'
 }
 
 module.exports = {
--- wrong_answer_messages.js
+++ wrong_answer_messages.js
@@ -1,14 +1,14 @@
 const modalpartikeln = {
-  denn: 'Falsch! Die richtige Antwort ist denn.\nWie geht es dir denn?\ndenn - nue bei Fragen (situatives Interesse)',
-  ruhig: 'Falsch! Die richtige Antwort ist ruhig.\nDu kannst ruhig so viel Jägermeister trinken, wie du willst.\nruhig - kein Problem',
-  halt: 'Falsch! Die richtige Antwort ist halt.\nWollen wir Döner essen? Nein, ich habe kein hunger. Dann gehe ich halt nach Hause.\nhalt - Allgemeingültigkeit, Resignatio',
-  aber: 'Falsch! Die richtige Antwort ist aber.\nDeutschland ist aber toll!\naber - sehr + adj (sehr/wirklich)',
-  einfach: 'Falsch! Die richtige Antwort ist einfach.\nEr lief einfach davon.\neinfach - ohne Weiteres/ ohne weiteren Grund macht man so',
-  ja: 'Falsch! Die richtige Antwort ist ja.\nDarüber haben wir ja schon besprochen\nja - Was bereits gesagt wurde',
-  vielleicht: 'Falsch! Die richtige Antwort ist vielleicht.\nDie Party war vielleicht toll.\nvielleicht - in der Tat sehr + adj. Das bekräftigt man seine Meinung',
-  mal: 'Falsch! Die richtige Antwort ist mal.\nkannst du mir mal helfen?\nmal - höfliches Imperativ',
-  eigentlich: 'Falsch! Die richtige Antwort ist eigentlich.\nHast du den Professor eigentlich schon gefragt?\neigentlich - Themawechsel',
-  doch: 'Falsch! Die richtige Antwort ist doch.\nVielleicht bestellen wir doch lieber Suhi?\ndoch - freundliche Bitten und Aufforderungen'
+  denn: 'Falsch! Die richtige Antwort ist "denn".\nDanke, mir geht es gut! Wie geht es dir denn?\ndenn - nue bei Fragen (situatives Interesse)',
+  ruhig: 'Falsch! Die richtige Antwort ist "ruhig".\nWir haben die Getränke-Flatrate bestellt. Du kannst ruhig so viel Jägermeister trinken, wie du willst.\nruhig - kein Problem',
+  halt: 'Falsch! Die richtige Antwort ist "halt".\nWollen wir noch einen Döner essen? - Nein, ich habe keinen Hunger. - Dann gehe ich halt nach Hause.\nhalt - Allgemeingültigkeit, Resignatio',
+  aber: 'Falsch! Die richtige Antwort ist "aber".\nEin Bier kostet nur 50 Cent? Deutschland ist aber toll!\naber - sehr + adj (sehr/wirklich)',
+  einfach: 'Falsch! Die richtige Antwort ist "einfach".\nIch wollte mit ihm reden, aber er lief einfach weg.\neinfach - ohne Weiteres/ ohne weiteren Grund macht man so',
+  ja: 'Falsch! Die richtige Antwort ist "ja".\nUnser Thema heute ist der Klimawandel. Darüber haben wir ja schon letzte Woche gesprochen.\nja - Was bereits gesagt wurde',
+  vielleicht: 'Falsch! Die richtige Antwort ist "vielleicht".\nWie war es gestern? - Die Party war vielleicht toll. Wir haben die ganze Nacht gefeiert.\nvielleicht - in der Tat sehr + adj. Das bekräftigt man seine Meinung',
+  mal: 'Falsch! Die richtige Antwort ist "mal".\nkannst du mir mal helfen?\nmal - höfliches Imperativ',
+  eigentlich: 'Falsch! Die richtige Antwort ist "eigentlich".\nHast du den Professor eigentlich schon gefragt?\neigentlich - Themawechsel',
+  doch: 'Falsch! Die richtige Antwort ist "doch".\nHmm, Pizza ist lecker, aber Inga mag gerne Reis. Vielleicht bestellen wir doch lieber Suhi?\ndoch - freundliche Bitten und Aufforderungen'
 }
 
 module.exports = {
